dfd51907d4971c489d73cb143833920e677bb270
Angela Woods <woods2007@gmail.com>
2014-12-20 13:49:45 -0500

    Almost have stock picker done...

---
+++ enumerate.rb
@@ -0,0 +1,35 @@
+module Enumerable
+	def my_each
+    # your code here
+  	end
+
+  	def my_each_with_index
+
+  	end
+
+  	def my_select
+  	
+  	end
+
+  	def my_all?
+  	
+  	end
+
+  	def my_any?
+  	
+  	end
+
+  	def my_none?
+  	
+  	end
+
+  	def my_map
+  	
+  	end
+
+  	def my_inject
+  	
+  	end
+end
+
+my
--- stock_picker.rb
+++ stock_picker.rb
@@ -1,2 +1,22 @@
-def stock_picker
+def stock_picker(stock_prices)
+	day = 0
+	buy_day = 0
+	sell_day = 0
+	max_profit = 0
+	last_index = stock_prices.length - 1 
+
+	(0..last_index - 1).each do |buy_index|
+		day_scanner = stock_prices[1..stock_prices.length - 1]
+		current_profit = day_scanner.max - stock_prices[buy_index]
+		stock_prices.each do |sell_index|
+			if current_profit > max_profit
+				max_profit = sell_index - buy_index
+				buy_day = buy_index
+				sell_day = sell_index
+			end
+		end		
+	end
+	puts "Buy when #{buy_day} and sell when #{sell_day} for a maximum profit of #{max_profit}"
 end
+
+stock_picker([17,3,6,9,15,8,6,1,10])
